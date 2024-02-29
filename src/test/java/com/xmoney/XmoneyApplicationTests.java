b8bc38aa60c1069f067baeb5eb5ea1eeb1ad7ddc
Caio Alves <caioanegocios@gmail.com>
2024-02-29 09:16:02 -0300

    Initial config

---
+++ src/test/java/com/xmoney/XmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
