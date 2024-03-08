103c91b8a937c67abf4ccbca0d195ecf56135210
Caio Alves <caioanegocios@gmail.com>
2024-03-08 20:39:49 -0300

    Create User entity

---
+++ src/main/java/com/xmoney/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.xmoney.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
